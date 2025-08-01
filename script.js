f554b62619bccea27e08e298d8a5e379484e64a8
Sana <matusalasana.gmail.com>
2025-08-01 18:27:41 +0300

    issue with spacing fixed

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ const checkBtn = document.getElementById("check-btn");
 
 textInput.addEventListener("input", function() {
     const inputText = textInput.value;
-    const reversedText = inputText.split('').reverse().join('');
+    const cleaned = inputText.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    const reversedText = inputText.split('').reverse().join('').replace(/[^A-Za-z0-9]/g, '').toLowerCase();
     checkBtn.onclick = function() {
-        if (inputText.length > 0 && (inputText== reversedText)) {
+        if (inputText.length > 0 && (cleaned== reversedText)) {
             resultText.textContent = `The word "${inputText}" is a palindrome.`;
             resultText.style.color = "green";
             resultText.style.fontWeight = "bold";
         } 
         else {
-            resultText.innerHTML = `The word "${inputText}" is not a palindrome.`;
+            resultText.textContent = `The word "${inputText}" is not a palindrome.`;
             resultText.style.color = "red";
             resultText.style.fontWeight = "bold";
         }
