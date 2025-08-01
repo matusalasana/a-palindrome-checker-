0408e6c5517241f63e9edc80f037c927d09f7a58
Sana <matusalasana.gmail.com>
2025-08-01 19:05:32 +0300

    issues with empty input fixed

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 const textInput = document.getElementById("text-input");
 const resultText = document.getElementById("result");
-const checkBtn = document.getElementById("check-btn");
+const checkBtn = document.getElementById("check-btn");    
 
-textInput.addEventListener("input", function() {
-    const inputText = textInput.value;
-    const cleaned = inputText.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
-    const reversedText = inputText.split('').reverse().join('').replace(/[^A-Za-z0-9]/g, '').toLowerCase();
     checkBtn.onclick = function() {
+
+        const inputText = textInput.value.trim();
+
+        if (inputText === ""){
+            resultText.textContent = "Please enter a word or phrase to check.";
+            resultText.style.color = "red";
+            resultText.style.fontWeight = "bold";
+            return;
+        }
+
+        const cleaned = inputText.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+        const reversedText = cleaned.split('').reverse().join('');
+
         if (inputText.length > 0 && (cleaned== reversedText)) {
             resultText.textContent = `The word "${inputText}" is a palindrome.`;
             resultText.style.color = "green";
-            resultText.style.fontWeight = "bold";
         } 
         else {
             resultText.textContent = `The word "${inputText}" is not a palindrome.`;
             resultText.style.color = "red";
-            resultText.style.fontWeight = "bold";
         }
+
+        resultText.style.fontWeight = "bold";
+
     };
-});
+
